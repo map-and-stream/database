9652407fdd0ff3911971df2abc9fa4db7f2b802a
majid <majid.asgari.cs@gmail.com>
2025-10-24 00:53:37 +0330

    using ConnectionConfig for generate connection string.

--- example/main.cpp
+++ example/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 
+#include "config.h"
 #include "postgresql.h"
 
 int main() {
+    ConnectionConfig cfg;
+    cfg.host = "localhost";
+    cfg.port = 5432;
+    cfg.dbname = "mydb";
+    cfg.user = "myuser";
+    cfg.password = "mypassword";
+    cfg.connect_timeout = 5;
+
     std::cout << "try to open connection..." << std::endl;
     PostgreSQL pg("postgresql://postgres:qazwsx@172.21.144.1:5432/mydb?connect_timeout=2");
-    std::cout << "result of open :" << pg.open();
+    // PostgreSQL pg(cfg.toPostgresConnection()); //TODO: use this way for create connection
+    std::cout << "result of open :" << pg.open() << std::endl;
 
     return 0;
 }
---
+++ src/config.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+struct ConnectionConfig {
+    std::string host;
+    int port = 5432;
+    std::string dbname;
+    std::string user;
+    std::string password;
+    int connect_timeout = 10;  // seconds
+
+    std::string toPostgresConnection() const {
+        // postgresql://postgres:qazwsx@172.21.144.1:5432/mydb?connect_timeout=2
+        return "postgresql://" + user + ":" + password + "@" + host + ":" + std::to_string(port) +
+               "/" + dbname + "?" + "connect_timeout=" + std::to_string(connect_timeout);
+    }
+};
