1fe20714718db2f2dec39075856c7740c6ef84d5
mohammad imani <mohammadimani1370@gmail.com>
2025-10-25 18:53:04 +0330

    add class connectionConfig

--- example/main.cpp
+++ example/main.cpp
@@ -5,16 +5,16 @@
 
 int main() {
     ConnectionConfig cfg;
-    cfg.host = "localhost";
+    cfg.host = "192.168.10.137";
     cfg.port = 5432;
-    cfg.dbname = "mydb";
-    cfg.user = "myuser";
-    cfg.password = "mypassword";
+    cfg.dbname = "khabirdb";
+    cfg.user = "javadshekarian";
+    cfg.password = "Isiran@123";
     cfg.connect_timeout = 5;
 
     std::cout << "try to open connection..." << std::endl;
-    PostgreSQL pg("postgresql://postgres:qazwsx@172.21.144.1:5432/mydb?connect_timeout=2");
-    // PostgreSQL pg(cfg.toPostgresConnection()); //TODO: use this way for create connection
+    // PostgreSQL pg("postgresql://postgres:qazwsx@172.21.144.1:5432/mydb?connect_timeout=2");
+    PostgreSQL pg(cfg.toPostgresConnection()); //TODO: use this way for create connection
     std::cout << "result of open :" << pg.open() << std::endl;
 
     return 0;
